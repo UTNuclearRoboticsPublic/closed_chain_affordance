d0e2f8806b64a4c3e24b44c02466bdfb3fdfde07
Crasun Jans <jpanthi@utexas.edu>
2023-10-26 21:04:07 -0500

    fix tf lookup issue

--- ros/cc_affordance_planner/src/cc_affordance_planner_node.cpp
+++ ros/cc_affordance_planner/src/cc_affordance_planner_node.cpp
@@ -63,52 +63,6 @@ private:
   geometry_msgs::TransformStamped
       transformStamped_; // ros message to hold transform info
 };
-/* geometry_msgs::TransformStamped */
-/*     transformStamped_; // ros message to hold transform info */
-/* Eigen::Isometry3d get_htm(tf2_ros::Buffer tfBuffer_, std::string
- * targetFrame_, */
-/*                           std::string referenceFrame_) { */
-
-/*   Eigen::Isometry3d htm; // Output */
-/*   // Query the listener for the desired transformation at the latest
- * available */
-/*   // time ros::Time::now() */
-/*   try { */
-/*     transformStamped_ = tfBuffer_.lookupTransform( */
-/*         targetFrame_, referenceFrame_, ros::Time(0), ros::Duration(0.2)); */
-/*   } catch (tf2::TransformException &ex) { */
-/*     ROS_WARN("%s", ex.what()); */
-/*   } */
-
-/*   // Convert the message to Eigen::Isometry3d type */
-/*   // Extract translation and rotation from the TransformStamped message */
-/*   Eigen::Vector3d translation(transformStamped_.transform.translation.x, */
-/*                               transformStamped_.transform.translation.y, */
-/*                               transformStamped_.transform.translation.z); */
-/*   Eigen::Quaterniond rotation(transformStamped_.transform.rotation.w, */
-/*                               transformStamped_.transform.rotation.x, */
-/*                               transformStamped_.transform.rotation.y, */
-/*                               transformStamped_.transform.rotation.z); */
-
-/*   // Set the translation and rotation components of the HTM */
-/*   htm.translation() = translation; */
-/*   htm.linear() = rotation.toRotationMatrix(); */
-
-/*   return htm; */
-/* } */
-
-/* Eigen::VectorXd htmToScrew(Eigen::Isometry3d htm) { */
-
-/*   /1* std::cout << "Here is the rotation of the HTM: \n" *1/ */
-/*   /1*           << htm.linear() << std::endl; *1/ */
-/*   /1* std::cout << "Here is the translation of the HTM: \n" *1/ */
-/*   /1*           << htm.translation() << std::endl; *1/ */
-/*   Eigen::VectorXd s(6); */
-/*   Eigen::Vector3d w = htm.linear().block<3, 1>(0, 2); */
-/*   s.head(3) = w; */
-/*   s.tail(3) = -w.cross(htm.translation()); */
-/*   return s; */
-/* } */
 int main(int argc, char **argv) {
   ros::init(argc, argv, "urdf_parser_example");
   ros::NodeHandle nh;
