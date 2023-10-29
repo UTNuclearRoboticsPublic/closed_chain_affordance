2f9c5fa1544916f3b1a62a422185faa17eaa8bc8
Crasun Jans <jpanthi@utexas.edu>
2023-10-29 00:21:04 -0500

    add moveit planning and execution: working demo on spot

--- ros/cc_affordance_planner/src/cc_affordance_planner_node.cpp
+++ ros/cc_affordance_planner/src/cc_affordance_planner_node.cpp
@@ -2,6 +2,9 @@
 #include <Eigen/Geometry>
 #include <cc_affordance_planner/cc_affordance_planner.hpp>
 #include <geometry_msgs/TransformStamped.h>
+#include <moveit/move_group_interface/move_group_interface.h>
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
+#include <moveit_visual_tools/moveit_visual_tools.h>
 #include <ros/ros.h>
 #include <tf2_ros/transform_listener.h>
 
@@ -65,10 +68,10 @@ private:
       transformStamped_; // ros message to hold transform info
 };
 int main(int argc, char **argv) {
-  ros::init(argc, argv, "urdf_parser_example");
-  ros::NodeHandle nh;
+  ros::init(argc, argv, "cc_affordance_planner_node");
+  ros::NodeHandle node_handle;
 
-  CcAffordancePlannerNode ccAffordancePlannerNode(nh);
+  CcAffordancePlannerNode ccAffordancePlannerNode(node_handle);
   //** Creation of screw axes
   //* Screw axis locations from base frame in home position
   // First get HTMs from TF data
@@ -101,15 +104,16 @@ int main(int argc, char **argv) {
   w.col(3) << 1, 0, 0;
   w.col(4) << 0, 1, 0;
   w.col(5) << 1, 0, 0;
-  w.col(6) << 1, 0, 0; // Imaginary joint
-  w.col(7) << 0, 1, 0; // Imaginary joint
-  w.col(8) << 0, 0, 1; // Imaginary joint
-  w.col(9) << 1, 0, 0; // Affordance
+  w.col(6) << 1, 0, 0;  // Imaginary joint
+  w.col(7) << 0, 1, 0;  // Imaginary joint
+  w.col(8) << 0, 0, 1;  // Imaginary joint
+  w.col(9) << -1, 0, 0; // Affordance
 
   Eigen::Isometry3d eeHtm =
       ccAffordancePlannerNode.get_htm(targetFrame_, "arm0_fingers");
   Eigen::Vector3d q_ee = eeHtm.translation();
-  Eigen::Vector3d q_aff = q_ee + Eigen::Vector3d(0.1, 0, 0);
+  const double affOffset = 0.1;
+  Eigen::Vector3d q_aff = q_ee + Eigen::Vector3d(0, affOffset, 0);
   q.col(6) = q_ee;  // imaginary joint
   q.col(7) = q_ee;  // imaginary joint
   q.col(8) = q_ee;  // imaginary joint
@@ -125,6 +129,9 @@ int main(int argc, char **argv) {
         q.col(i); // required to convert to VectorXd type for use with cross
     slist.col(i) << wcurr, -wcurr.cross(qcurr);
   }
+
+  // Pure translation edit:
+  /* slist.col(9) = (Eigen::VectorXd(6) << 0, 0, 1, 0, 0, 0).finished(); */
   std::cout << "w: \n" << w << std::endl;
   std::cout << "q: \n" << q << std::endl;
 
@@ -142,10 +149,249 @@ int main(int argc, char **argv) {
   if (solution.empty())
     std::cout << "No solution found" << std::endl;
   else
-    std::cout << "Here is the first point in the solution \n"
+    std::cout << "Solution length: " << solution.size()
+              << "\nHere is the first point in the solution \n"
               << solution.at(0) << std::endl;
 
   // Send it to MoveIt for planning
+  /* ros::AsyncSpinner spinner(1); */
+  /* spinner.start(); */
+  /* moveit::planning_interface::MoveGroupInterface move_group("arm"); */
+
+  /* // Set joint trajectory */
+  /* std::vector<double> joint_values; */
+  /* const size_t armDoF = 6; */
+
+  /* for (size_t i = 0; i < armDoF; i++) { */
+  /*   joint_values.push_back(solution.at(0)[i]); */
+  /*   std::cout << "joint_value[" << i << "] = " << joint_values[i] <<
+   * std::endl; */
+  /* } */
+
+  /* move_group.setJointValueTarget(joint_values); */
+
+  /* // Plan */
+  /* moveit::planning_interface::MoveGroupInterface::Plan my_plan; */
+  /* moveit::core::MoveItErrorCode planning_result = move_group.plan(my_plan);
+   */
+
+  /* if (planning_result == moveit::core::MoveItErrorCode::SUCCESS) { */
+  /*   // The planning was successful, and you can execute it. */
+  /*   /1* move_group.execute(my_plan); *1/ */
+  /*   ROS_INFO("planning succeeded"); */
+  /* } else { */
+  /*   // Planning failed; you may want to handle the failure accordingly. */
+  /*   ROS_ERROR("Planning failed "); */
+  /* } */
+
+  /* /1* if (success) { *1/ */
+  /* /1*   move_group.execute(my_plan); *1/ */
+  /* /1* } *1/ */
+
+  /* // Visualize the plan in Rviz */
+  /* moveit_visual_tools::MoveItVisualTools visual_tools( */
+  /*     "move_group/monitored_planning_scene"); */
+  /* visual_tools.loadRemoteControl(); */
+  /* visual_tools.publishPath(my_plan.trajectory_, */
+  /*                          rviz_visual_tools::colors::LIME_GREEN, */
+  /*                          rviz_visual_tools::scales::SMALL, "arm"); */
+  /* visual_tools.publishPath(my_plan.trajectory_,
+   * rviz_visual_tools::colors::BLUE, */
+  /*                          rviz_visual_tools::scales::SMALL); */
+  /* visual_tools.trigger(); */
+
+  /* ros::shutdown(); */
+
+  // new attempt
+  ros::AsyncSpinner spinner(1);
+  spinner.start();
+
+  // BEGIN_TUTORIAL
+  //
+  // Setup
+  // ^^^^^
+  //
+  // MoveIt operates on sets of joints called "planning groups" and stores them
+  // in an object called the `JointModelGroup`. Throughout MoveIt the terms
+  // "planning group" and "joint model group" are used interchangeably.
+  static const std::string PLANNING_GROUP = "arm";
+
+  // The :planning_interface:`MoveGroupInterface` class can be easily
+  // setup using just the name of the planning group you would like to control
+  // and plan for.
+  moveit::planning_interface::MoveGroupInterface move_group_interface(
+      PLANNING_GROUP);
+
+  // We will use the :planning_interface:`PlanningSceneInterface`
+  // class to add and remove collision objects in our "virtual world" scene
+  moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
+
+  // Raw pointers are frequently used to refer to the planning group for
+  // improved performance.
+  const moveit::core::JointModelGroup *joint_model_group =
+      move_group_interface.getCurrentState()->getJointModelGroup(
+          PLANNING_GROUP);
+
+  // Visualization
+  // ^^^^^^^^^^^^^
+  //
+  // The package MoveItVisualTools provides many capabilities for visualizing
+  // objects, robots, and trajectories in RViz as well as debugging tools such
+  // as step-by-step introspection of a script.
+  namespace rvt = rviz_visual_tools;
+  moveit_visual_tools::MoveItVisualTools visual_tools("arm0_base_link");
+  visual_tools.deleteAllMarkers();
+
+  // Remote control is an introspection tool that allows users to step through a
+  // high level script via buttons and keyboard shortcuts in RViz
+  visual_tools.loadRemoteControl();
+
+  // RViz provides many types of markers, in this demo we will use text,
+  // cylinders, and spheres
+  Eigen::Isometry3d text_pose = Eigen::Isometry3d::Identity();
+  text_pose.translation().z() = 1.0;
+  visual_tools.publishText(text_pose, "MoveGroupInterface Demo", rvt::WHITE,
+                           rvt::XLARGE);
+
+  // Batch publishing is used to reduce the number of messages being sent to
+  // RViz for large visualizations
+  visual_tools.trigger();
+
+  // Getting Basic Information
+  // ^^^^^^^^^^^^^^^^^^^^^^^^^
+  //
+  // We can print the name of the reference frame for this robot.
+  ROS_INFO_NAMED("tutorial", "Planning frame: %s",
+                 move_group_interface.getPlanningFrame().c_str());
+
+  // We can also print the name of the end-effector link for this group.
+  ROS_INFO_NAMED("tutorial", "End effector link: %s",
+                 move_group_interface.getEndEffectorLink().c_str());
+
+  // We can get a list of all the groups in the robot:
+  ROS_INFO_NAMED("tutorial", "Available Planning Groups:");
+  std::copy(move_group_interface.getJointModelGroupNames().begin(),
+            move_group_interface.getJointModelGroupNames().end(),
+            std::ostream_iterator<std::string>(std::cout, ", "));
+
+  // Start the demo
+  // ^^^^^^^^^^^^^^^^^^^^^^^^^
+  visual_tools.prompt(
+      "Press 'next' in the RvizVisualToolsGui window to start the demo");
+
+  // .. _move_group_interface-planning-to-pose-goal:
+  //
+  /* // Planning to a Pose goal */
+  /* // ^^^^^^^^^^^^^^^^^^^^^^^ */
+  /* // We can plan a motion for this group to a desired pose for the */
+  /* // end-effector. */
+  /* geometry_msgs::Pose target_pose1; */
+  /* target_pose1.orientation.w = 1.0; */
+  /* target_pose1.position.x = 0.28; */
+  /* target_pose1.position.y = -0.2; */
+  /* target_pose1.position.z = 0.5; */
+  /* move_group_interface.setPoseTarget(target_pose1); */
+
+  /* // Now, we call the planner to compute the plan and visualize it. */
+  /* // Note that we are just planning, not asking move_group_interface */
+  /* // to actually move the robot. */
+  moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+
+  /* bool success = (move_group_interface.plan(my_plan) == */
+  /*                 moveit::core::MoveItErrorCode::SUCCESS); */
+
+  /* ROS_INFO_NAMED("tutorial", "Visualizing plan 1 (pose goal) %s", */
+  /*                success ? "" : "FAILED"); */
+
+  /* // Visualizing plans */
+  /* // ^^^^^^^^^^^^^^^^^ */
+  /* // We can also visualize the plan as a line with markers in RViz. */
+  /* ROS_INFO_NAMED("tutorial", "Visualizing plan 1 as trajectory line"); */
+  /* visual_tools.publishAxisLabeled(target_pose1, "pose1"); */
+  /* visual_tools.publishText(text_pose, "Pose Goal", rvt::WHITE, rvt::XLARGE);
+   */
+  /* visual_tools.publishTrajectoryLine(my_plan.trajectory_, joint_model_group);
+   */
+  /* visual_tools.trigger(); */
+  /* visual_tools.prompt( */
+  /*     "Press 'next' in the RvizVisualToolsGui window to continue the demo");
+   */
+
+  // Finally, to execute the trajectory stored in my_plan, you could use the
+  // following method call: Note that this can lead to problems if the robot
+  // moved in the meanwhile. move_group_interface.execute(my_plan);
+
+  // Moving to a pose goal
+  // ^^^^^^^^^^^^^^^^^^^^^
+  //
+  // If you do not want to inspect the planned trajectory,
+  // the following is a more robust combination of the two-step plan+execute
+  // pattern shown above and should be preferred. Note that the pose goal we had
+  // set earlier is still active, so the robot will try to move to that goal.
+
+  // move_group_interface.move();
+
+  // Planning to a joint-space goal
+  // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  //
+  // Let's set a joint space goal and move towards it.  This will replace the
+  // pose target we set above.
+  //
+  // To start, we'll create an pointer that references the current robot's
+  // state. RobotState is the object that contains all the current
+  // position/velocity/acceleration data.
+  visual_tools.trigger();
+  visual_tools.prompt("Press 'next' to plan and execute the trajectory");
+  for (size_t j = 0; j < solution.size(); j++) {
+    /* moveit::core::RobotStatePtr current_state = */
+    /*     move_group_interface.getCurrentState(); */
+    //
+    // Next get the current set of joint values for the group.
+    /* std::vector<double> joint_group_positions_cur; */
+    /* current_state->copyJointGroupPositions(joint_model_group, */
+    /*                                        joint_group_positions_cur); */
+    std::vector<double> joint_group_positions;
+    const size_t armDoF = 6;
+
+    /* visual_tools.trigger(); */
+    /* visual_tools.prompt("Press 'next' to plan the trajectory"); */
+    for (size_t i = 0; i < armDoF; i++) {
+      /* joint_group_positions.push_back(joint_group_positions_cur[i] + */
+      /*                                 solution.at(j)[i]); */
+      joint_group_positions.push_back(solution.at(j)[i]);
+      std::cout << "joint_value[" << i << "] = " << joint_group_positions[i]
+                << std::endl;
+    }
+
+    // Now, let's modify one of the joints, plan to the new joint space goal and
+    // visualize the plan.
+    /* joint_group_positions[0] = -tau / 6; // -1/6 turn in radians */
+    move_group_interface.setJointValueTarget(joint_group_positions);
+
+    // We lower the allowed maximum velocity and acceleration to 5% of their
+    // maximum. The default values are 10% (0.1). Set your preferred defaults in
+    // the joint_limits.yaml file of your robot's moveit_config or set explicit
+    // factors in your code if you need your robot to move faster.
+    move_group_interface.setMaxVelocityScalingFactor(0.05);
+    move_group_interface.setMaxAccelerationScalingFactor(0.05);
+
+    bool success = (move_group_interface.plan(my_plan) ==
+                    moveit::core::MoveItErrorCode::SUCCESS);
+    ROS_INFO_NAMED("tutorial", "Visualizing plan 2 (joint space goal) %s",
+                   success ? "" : "FAILED");
+
+    // Visualize the plan in RViz
+    visual_tools.deleteAllMarkers();
+    visual_tools.publishText(text_pose, "Joint Space Goal", rvt::WHITE,
+                             rvt::XLARGE);
+    visual_tools.publishTrajectoryLine(my_plan.trajectory_, joint_model_group);
+    /* visual_tools.trigger(); */
+    /* visual_tools.prompt("Press 'next' to execute the trajectory"); */
+    if (success) {
+      move_group_interface.execute(my_plan);
+    }
+    /* joint_group_positions.clear(); */
+  }
 
   return 0;
 }
