6bf31488fa4a052bd61ee4732a992a328bde5095
Crasun Jans <jpanthi@utexas.edu>
2023-11-11 15:27:11 -0600

    add moveit plan and viz server skeleton and its custom srv

---
+++ ros/cc_affordance_planner/src/cc_affordance_planner_moveit_plan_and_viz_server.cpp
@@ -0,0 +1,9 @@
+#include <cc_affordance_planner/MoveItPlanAndViz.h>
+#include <ros/ros.h>
+
+int main(int argc, char **argv) {
+
+  ros::init(argc, argv, "MoveIt_Planning_Server");
+  ROS_INFO("Hello Service World");
+  return 0;
+}
