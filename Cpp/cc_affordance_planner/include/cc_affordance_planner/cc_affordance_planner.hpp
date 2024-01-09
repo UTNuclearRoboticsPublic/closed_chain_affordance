677db048b889e91e36e9a7f31d44c1f164c3bc42
cjans121 <jpanthi@utexas.edu>
2024-01-08 16:27:47 -0800

    Update Doxygen documentation

--- Cpp/cc_affordance_planner/include/cc_affordance_planner/cc_affordance_planner.hpp
+++ Cpp/cc_affordance_planner/include/cc_affordance_planner/cc_affordance_planner.hpp
@@ -26,67 +26,89 @@ struct PlannerResult
 };
 
 /**
- * @brief Given a list of closed-chain screw axes, corresponding starting joint
- * angles, and affordance goal, this planner computes the inverse kinematics
- * joint trajectory solution.
+ * @brief Differential joint trajectory planner for a given affordance.
+ *        Construct this planner with an empty constructor and set the following planner parameters, which are available
+ *        as public variables:
+ *
+ *        - Use the parameter p_aff_step_deltatheta_a to specify the trajectory density with an affordance step.
+ *          For example, an affordance goal of 0.5 rad could have an affordance step of 0.1 rad,
+ *          resulting in a trajectory with 5 points, where from start to end, the intermediate affordance goals
+ *          are 0.1, 0.2, 0.3, 0.4, and 0.5 rad.
+ *
+ *        - Another important parameter is p_task_err_threshold_eps_s, representing the error threshold
+ *          for the affordance step. For instance, if 10% accuracy is desired for a 0.1 rad step, set the error
+ * 	    threshold to 0.01 rad.
+ *
+ *        - Advanced users can utilize two additional parameters:
+ *          - p_closure_err_threshold_eps_r: Specify the error threshold for the closed-chain closure error.
+ *          - p_max_itr_l: Specify the maximum iterations for the closed-chain inverse kinematics solver.
+ *
+ * @note The provided parameters allow users to control the trajectory density, accuracy, and solver behavior.
  */
+
 class CcAffordancePlanner
 {
   public:
-    //* Algorithm control parameters (non-const to adjust from outside)
-    // For stepper loop
+    // See Doxygen brief above
     double p_aff_step_deltatheta_a = 0.1;
-    // For IK loop
-    /* double accuracy = 10.0 * (1.0 / 100.0); // accuracy for error threshold */
     double p_task_err_threshold_eps_s = 0.01;
     double p_closure_err_threshold_eps_r = 1e-6;
     int p_max_itr_l = 50;
 
     // Constructor
-    /**
-     * @brief Given a list of closed-chain screw axes, corresponding starting
-     joint angles, and affordance goal, this planner computes the inverse
-     kinematics joint trajectory solution.
-     *
-     * @param slist List of all closed-chain screw axes
-     * @param thetalist0 Corresponding joint angles for starting configuration
-     * @param affGoal Desired affordance goal
-     */
     CcAffordancePlanner();
 
     // Methods
     /**
-     * @brief Steps the IK solver towards the affordance goal affGoal by an
-     * affordance step (p_aff_step_deltatheta_a)
+     * @brief After setting the planner parameters described in the class documentation, call this function to generate
+     * the differential joint trajectory to execute a given affordance.
      *
-     * @return Struct containing the result of the planning with fields:
-     success of type bool, traj_full_or_partial of type string with values: "Full",
-     "Partial", or "Unset", and joint_traj of type std::vector<Eigen::VectorXd
+     * @param slist Eigen::MatrixXd containing as columns 6x1 Screws representing all joints of the closed-chain model,
+     * i.e. robot joints, virtual ee joint, affordance joint
+     * @param theta_adf Affordance goal for the affordance
+     * @param task_offset_tau A numeric parameter indicating the length of the secondary joint vector.
+     *                        The value 1 implies only affordance control, 2 represents affordance control
+     *                        along with fixing the gripper x-axis, 3 involves fixing the gripper x and y axes,
+     *                        and 4 involves fixing the gripper x, y, and z axes.
+     *
+     * @return Struct containing the result of the planning with fields: success of type bool, traj_full_or_partial of
+     * type string with values: "Full", "Partial", or "Unset", and joint_traj of type std::vector<Eigen::VectorXd
+     * containing joint trajectory values for all joints
      */
-
     PlannerResult affordance_stepper(const Eigen::MatrixXd &slist, const double &theta_adf,
                                      const size_t &task_offset_tau);
     /**
-     * @brief Closed-chain IK solver. Not standalone.
+     * @brief Given a list of closed-chain Screws, initial primary and secondary joint guesses, and desired secondary
+     * joint goals, computes the closed-chain inverse kinematics solution.
      *
-     * @return
+     * @param slist Eigen::MatrixXd containing as columns 6x1 Screws of the closed-chain mechanism
+     * @param theta_pg Eigen::VectorXd containing guesses for the primary joints
+     * @param theta_sg Eigen::VectorXd containing guesses for the secondary joints
+     * @param theta_sd Eigen::VectorXd containing goals for the secondary joints
+     *
+     * @return Eigen::VectorXd containing the inverse kinematics solution (including the exact secondary joint goals as
+     * well)
      */
-    std::optional<Eigen::VectorXd> cc_ik_solver(const Eigen::MatrixXd &slist, const Eigen::VectorXd &theta_p,
+    std::optional<Eigen::VectorXd> cc_ik_solver(const Eigen::MatrixXd &slist, const Eigen::VectorXd &theta_pg,
                                                 const Eigen::VectorXd &theta_sg, const Eigen::VectorXd &theta_sd);
 
     /**
-     * @brief Closed-chain error corrector. Not standalone.
+     * @brief Given a list of closed-chain screw axes, primary and secondary network matrices, and primary and secondary
+     * joint angles, returns joint angles corrected for closed-chain closure error
+     *
+     * @param slist Eigen::MatrixXd containing as columns 6x1 Screws of the closed-chain mechanism
+     * @param Np Eigen::MatrixXd representing primary network matrix
+     * @param Ns Eigen::MatrixXd representing secondary network matrix
+     * @param theta_p Eigen::VectorXd containing primary joint angles
+     * @param theta_s Eigen::VectorXd containing secondary joint angles
      */
     void closure_error_optimizer(const Eigen::MatrixXd &slist, const Eigen::MatrixXd &Np, const Eigen::MatrixXd &Ns,
                                  Eigen::VectorXd &theta_p,
-                                 Eigen::VectorXd &theta_sg); // theta_sg and theta_p returned by referece
+                                 Eigen::VectorXd &theta_s); // theta_sg and theta_p returned by referece
 
   private:
     constexpr static size_t twist_length_ = 6; // length of a twist vector
-
-    // Helper variables to share info between functions
-    /* Eigen::VectorXd thetalist_; // helper vector variable holding theta_p, theta_sg */
-    size_t nof_pjoints_;
-    size_t nof_sjoints_;
+    size_t nof_pjoints_;                       // number of primary joints
+    size_t nof_sjoints_;                       // number of secondary joints
 };
 #endif // CC_AFFORDANCE_PLANNER
