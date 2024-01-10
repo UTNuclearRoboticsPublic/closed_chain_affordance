fefc703f1fbe80880f4c63c9489e9170de66c02f
cjans121 <jpanthi@utexas.edu>
2024-01-09 16:12:31 -0800

    backup changes to cc_affordance_planner_ros

--- ros/cc_affordance_planner_ros/src/cc_affordance_planner_ros_node.cpp
+++ ros/cc_affordance_planner_ros/src/cc_affordance_planner_ros_node.cpp
@@ -4,155 +4,44 @@
 #include <affordance_util/affordance_util.hpp>
 #include <affordance_util_ros/affordance_util_ros.hpp>
 #include <cc_affordance_planner/cc_affordance_planner.hpp>
-#include <filesystem>
-#include <moveit_plan_and_viz/MoveItPlanAndViz.h>
-#include <ros/ros.h>
-#include <tf2_ros/transform_listener.h>
-
 #include <cmath>
 #include <control_msgs/FollowJointTrajectoryAction.h>
-#include <ros/package.h>
+#include <moveit_plan_and_viz/MoveItPlanAndViz.h>
+#include <ros/ros.h>
 #include <sensor_msgs/JointState.h>
+#include <tf2_ros/transform_listener.h>
 /*
    Author: Crasun Jans
 */
 
-class CustomException : public std::exception
+class CcAffordancePlannerRos
 {
   public:
-    CustomException(const char *message) : msg(message) {}
-
-    // Override the what() function to provide a description of the exception
-    virtual const char *what() const noexcept override { return msg.c_str(); }
-
-  private:
-    std::string msg;
-};
-
-std::string get_filepath_inside_pkg(const std::string &package_name, const std::string &rel_dir,
-                                    const std::string &filename)
-{
-
-    std::string full_filepath; // output of the function
-
-    // Get the path to the package
-    const std::string package_path = ros::package::getPath(package_name);
-
-    // Build full filepath and check for errors
-    if (!package_path.empty())
-    {
-        full_filepath = package_path + rel_dir + filename;
-    }
-    else
-    {
-        throw CustomException(("Failed to find path for package '" + package_name).c_str());
-    }
-
-    // Make sure the file exists
-    if (!std::filesystem::exists(full_filepath))
-    {
-        throw CustomException(("File does not exist at this path: '" + full_filepath).c_str());
-    }
-
-    return full_filepath;
-}
-Eigen::Matrix<double, 6, 1> get_screw(const Eigen::Vector3d &w, const Eigen::Vector3d &q)
-{
-
-    Eigen::VectorXd screw(6); // Output of the function
-
-    screw.head(3) = w;
-    screw.tail(3) = -w.cross(q); // q cross w
-
-    return screw;
-}
-Eigen::MatrixXd compose_cc_model_slist(const Eigen::MatrixXd &robot_slist, const Eigen::VectorXd &thetalist,
-                                       const Eigen::Matrix4d &M, const Eigen::Matrix<double, 6, 1> &aff_screw)
-{
-
-    // Compute robot Jacobian
-    Eigen::MatrixXd robot_jacobian = AffordanceUtil::JacobianSpace(robot_slist, thetalist);
-
-    // Append virtual screw axes as well as the affordance screw. TODO: Use home
-    // config screws and angles so as to track true gripper orientation
-    Eigen::MatrixXd app_slist(6, 4);
-
-    // Virtual screws
-    const Eigen::Matrix4d ee_htm = AffordanceUtil::FKinSpace(M, robot_slist, thetalist);
-    const Eigen::Vector3d q_vir = ee_htm.block<3, 1>(0, 3);
-    // Alternative method using TF data
-    /* tf2_ros::Buffer tf_buffer; */
-    /* const Eigen::Isometry3d ee_htm = */
-    /*     AffordanceUtilROS::get_htm("arm0_base_link", "arm0_tool0", tf_buffer);
-     */
-    /* const Eigen::Vector3d q_vir = ee_htm.translation(); */
-
-    Eigen::Matrix<double, 3, 3> w_vir;
-    w_vir.col(0) << 1, 0, 0; // x
-    w_vir.col(1) << 0, 1, 0; // y
-    w_vir.col(2) << 0, 0, 1; // z
-
-    for (size_t i = 0; i < w_vir.cols(); ++i)
-    {
-        app_slist.col(i) = get_screw(w_vir.col(i), q_vir);
-    }
-
-    // Affordance screw
-    app_slist.col(3) = aff_screw;
-
-    // Putting altogether
-    Eigen::MatrixXd slist(robot_slist.rows(), (robot_slist.cols() + app_slist.cols()));
-    slist << robot_jacobian, app_slist;
-
-    return slist;
-}
-
-// Get affordance screw
-Eigen::Matrix<double, 6, 1> get_aff_screw_from_tag(const Eigen::Vector3d &w_aff, const std::string &reference_frame,
-                                                   const std::string &affordance_frame, tf2_ros::Buffer &tf_buffer)
-{
-
-    // Extract end-effector location from TF data
-    Eigen::Vector3d q_aff;
-    const Eigen::Isometry3d tag_htm = AffordanceUtilROS::get_htm(reference_frame, affordance_frame, tf_buffer);
-    q_aff = tag_htm.translation();
-    std::cout << "Here is the affordance frame location: \n" << q_aff << std::endl;
-
-    return get_screw(w_aff, q_aff); // return screw axis
-}
-
-class CcAffordancePlannerRosNode
-{
-  public:
-    CcAffordancePlannerRosNode(const std::string &robot_config_file_path, const Eigen::VectorXd &aff_screw,
-                               const double &aff_goal)
+    CcAffordancePlannerRos(const std::string &robot_config_file_path, const Eigen::VectorXd &aff_screw,
+                           const double &aff_goal)
         : nh_("~"),
-          aff_screw_(aff_screw),
-          aff_goal_(aff_goal),
-          client_("/spot_arm/arm_controller/follow_joint_trajectory", true)
+          aff_screw(aff_screw),
+          aff_goal(aff_goal),
+          traj_execution_client_("/spot_arm/arm_controller/follow_joint_trajectory", true),
+          moveit_plan_and_viz_client_("/moveit_plan_and_viz_server", true)
     {
 
         // Subscribers
-        joint_states_sub_ = nh_.subscribe("/joint_states", 1000, &CcAffordancePlannerRosNode::joint_states_cb_, this);
+        joint_states_sub_ = nh_.subscribe("/joint_states", 1000, &CcAffordancePlannerRos::joint_states_cb_, this);
 
         // Extract robot config info
         const AffordanceUtil::RobotConfig &robotConfig = AffordanceUtil::robot_builder(robot_config_file_path);
-        h_slist_ = robotConfig.Slist;
+        robot_slist_ = robotConfig.Slist;
         M_ = robotConfig.M;
         tool_name_ = robotConfig.tool_name;
-        const size_t real_robot_joints = robotConfig.joint_names.size() - 3; // Disregard virtual joint names
-        joint_names_.assign(robotConfig.joint_names.begin(), robotConfig.joint_names.begin() + real_robot_joints);
-
-        // Create action client
-        /* client_("/spot_arm/arm_controller/follow_joint_trajectory", true); */
+        joint_names_ = robotConfig.joint_names;
     }
 
-    ~CcAffordancePlannerRosNode() {}
+    ~CcAffordancePlannerRos() {}
 
     void run_cc_affordance_planner()
     {
 
-        // Read joint states
         // Capture initial configuration joint states
         std::string capture_joint_states_conf;
         std::cout << "Put the robot in the start configuration for affordance "
@@ -175,60 +64,44 @@ class CcAffordancePlannerRosNode
         }
 
         // Manually set joint_states for planning time computation purposes only
-        joint_states_.positions <<
+        /* joint_states_.positions << */
 
-            std::cout << "Here are the captured joint states: \n"
-                                << joint_states_.positions << std::endl;
+        std::cout << "Here are the captured joint states: \n" << joint_states_.positions << std::endl;
 
-        // Extract robot screw list
-        Eigen::MatrixXd robot_slist = h_slist_.leftCols(joint_states_.positions.size());
         // Compose cc model and affordance goal
-        Eigen::MatrixXd cc_slist = compose_cc_model_slist(robot_slist, joint_states_.positions, M_, aff_screw_);
+        Eigen::MatrixXd cc_slist =
+            AffordanceUtilROS::compose_cc_model_slist(robot_slist_, joint_states_.positions, M_, aff_screw);
         std::cout << "Here is the full cc slist: \n" << cc_slist << std::endl;
 
-        // Call planner
-        // Define affordance goal and create planner object
-        /* const double affGoal = 1.0 * M_PI; */
-        /* const double affGoal = 0.1; */
-        Eigen::VectorXd thetalist0 = Eigen::VectorXd::Zero(cc_slist.cols());
-        /* CcAffordancePlanner ccAffordancePlanner(cc_slist, thetalist0, aff_goal_); */
-        auto sign_of = [](double x) {
-            return (x > 0) ? 1.0 : (x < 0) ? -1.0 : 0.0;
-        }; // to check the sign of affordance goal
-        /* ccAffordancePlanner.affStep = sign_of(aff_goal_) * 0.2; // should have the same sign as aff_goal_ */
-        /* ccAffordancePlanner.taskOffset = 3; */
-
-        // Construct the planner object
+        // Construct the CcAffordancePlanner object
         CcAffordancePlanner ccAffordancePlanner;
 
-        // Set algorithm parameters
-        const double aff_step = 0.01;
-        const double accuracy = 10.0 / 100.0;
-        ccAffordancePlanner.p_aff_step_deltatheta_a = sign_of(aff_goal_) * aff_step;
-        ccAffordancePlanner.p_task_err_threshold_eps_s = accuracy * aff_step;
-        const int task_offset = 1;
+        // Set planner parameters
+        auto sign_of = [](double x) {
+            return (x > 0) ? 1.0 : (x < 0) ? -1.0 : 0.0;
+        }; // Helper lambda to check the sign of affordance goal
 
-        // Run the planner
-        PlannerResult plannerResult = ccAffordancePlanner.affordance_stepper(cc_slist, aff_goal_, task_offset);
+        const double aff_step = 0.01;         // To be hard-coded as needed
+        const double accuracy = 10.0 / 100.0; // To be hard-coded as needed
+
+        ccAffordancePlanner.p_aff_step_deltatheta_a = sign_of(aff_goal) * aff_step;
+        ccAffordancePlanner.p_task_err_threshold_eps_s = accuracy * aff_step;
+
+        // Run the planner by passing the screw list, affordance goal, and task offset
+        const int gripper_control_par_tau = 1;
+        PlannerResult plannerResult =
+            ccAffordancePlanner.affordance_stepper(cc_slist, aff_goal, gripper_control_par_tau);
 
         // Print the first point in the trajectory if planner succeeds
         std::vector<Eigen::VectorXd> solution = plannerResult.joint_traj;
         if (plannerResult.success)
         {
-            std::cout << "Planner succeeded with "
-                      << plannerResult.traj_full_or_partial
-                      /* << " solution. and here is the first point in the trajectory \n" */
-                      /* << solution.at(0) << std::endl; */
-                      << " solution\n"
-                      << std::endl;
-            /* std::cout << "Here is the trajectory \n"; */
-            /* for (size_t i = 0; i < solution.size(); i++) { */
-            /*   std::cout << solution.at(i) << std::endl; */
-            /* } */
+            std::cout << "Planner succeeded with " << plannerResult.traj_full_or_partial
+                      << " solution, and planning took " << plannerResult.planning_time << " microseconds" << std::endl;
         }
         else
         {
-            std::cout << "No solution found" << std::endl;
+            std::cout << "Planner did not find a solution" << std::endl;
         }
 
         // Convert the solution trajectory to ROS message type
@@ -240,28 +113,21 @@ class CcAffordancePlannerRosNode
                              // contains qs data too
 
         // Visualize plan
-        // Send to move_plan_and_viz_server to visualize planning
-        std::string plan_and_viz_service_name = "/moveit_plan_and_viz_server";
-        ros::ServiceClient moveit_plan_and_viz_client =
-            nh_.serviceClient<moveit_plan_and_viz::MoveItPlanAndViz>(plan_and_viz_service_name);
-
         moveit_plan_and_viz::MoveItPlanAndViz moveit_plan_and_viz_goal;
         moveit_plan_and_viz_goal.request.joint_traj = goal.trajectory;
 
-        ROS_INFO_STREAM("Waiting for " << plan_and_viz_service_name << " service to be available");
-        ros::Duration timeout(60.0);
-        if (!ros::service::waitForService(plan_and_viz_service_name))
+        ros::Duration timeout(60.0); // 1 minute
+        if (!moveit_plan_and_viz_client_.waitForServer(timeout))
         {
-            ROS_INFO_STREAM(plan_and_viz_service_name << " service isn't available. Timed out waiting");
-        }
-        else
-        {
-            ROS_INFO_STREAM(plan_and_viz_service_name << " service found. Now sending goal.");
+            std::cout << "Could not find service within the timeout" << std::endl;
+            return;
         }
 
-        if (moveit_plan_and_viz_client.call(moveit_plan_and_viz_goal))
+        if (moveit_plan_and_viz_client_.call(moveit_plan_and_viz_goal))
         {
-            ROS_INFO_STREAM("Calling " << plan_and_viz_service_name << " service was successful.");
+            std::cout << "Calling " << plan_and_viz_service_name << " service was successful." << std::endl;
+
+            // Execute trajectory on the real robot
             std::string execution_conf;
             std::cout << "Ready to execute the trajectory? y to confirm" << std::endl;
             std::cin >> execution_conf;
@@ -272,40 +138,45 @@ class CcAffordancePlannerRosNode
                 return;
             }
 
-            // Execute directly
-            // Create the connection to the action server
-            /* ROS_INFO_STREAM("Waiting for server"); */
-            std::cout << "Waiting for server" << std::endl;
-            client_.waitForServer(); // Waits until the action server is up and
-            /* running */
+            if (!traj_execution_client_.waitForServer(timeout))
+            {
+                std::cout << "Could not find follow joint trajectory action server within the timeout" << std::endl;
+                return;
+            }
 
-            /* ROS_INFO_STREAM("Sending goal to action server for execution"); */
             std::cout << "Sending goal to action server for execution" << std::endl;
-            client_.sendGoal(goal);
-            client_.waitForResult(timeout);
+
+            traj_execution_client_.sendGoal(goal);
+
+            if (!traj_execution_client_.waitForResult(timeout))
+            {
+                std::cout << "Follow joint trajectory action server did not return result within timeout" << std::endl;
+                return;
+            }
         }
 
         else
         {
-            ROS_INFO_STREAM("Failed to call " << plan_and_viz_service_name << " service.");
+            std::cout << "Failed to call " << plan_and_viz_service_name << " service." << std::endl;
         }
-        // Execute plan
     }
 
   private:
     // ROS variables
     ros::NodeHandle nh_;
-    ros::Subscriber joint_states_sub_;
-    AffordanceUtilROS::JointTrajPoint joint_states_;
+    ros::Subscriber joint_states_sub_; // Joint states subscriber
+    ros::ServiceClient<moveit_plan_and_viz::MoveItPlanAndViz>
+        moveit_plan_and_viz_client_; // Service client to visualize joint trajectory
+    actionlib::SimpleActionClient<control_msgs::FollowJointTrajectoryAction>
+        traj_execution_client_; // Action client to execute joint trajectory on robot
+
+    AffordanceUtilROS::JointTrajPoint joint_states_; // Processed and ordered joint states data
+
     // Robot data
-    Eigen::MatrixXd h_slist_;
+    Eigen::MatrixXd robot_slist_;
     std::vector<std::string> joint_names_;
     Eigen::MatrixXd M_;
     std::string tool_name_;
-    // Affordance data
-    Eigen::Matrix<double, 6, 1> aff_screw_;
-    double aff_goal_;
-    actionlib::SimpleActionClient<control_msgs::FollowJointTrajectoryAction> client_;
 
     // Callback function for the joint_states subscriber
     void joint_states_cb_(const sensor_msgs::JointState::ConstPtr &msg)
@@ -328,47 +199,48 @@ int main(int argc, char **argv)
     const std::string rel_dir = "/config/";
     const std::string filename = package_name + ".yaml";
     std::string robot_cc_description_path;
-    try
-    {
-        robot_cc_description_path = get_filepath_inside_pkg(package_name, rel_dir, filename);
-    }
-    catch (const CustomException &e)
-    {
-        // Catch and handle the custom exception
-        std::cerr << "Caught exception: " << e.what() << std::endl;
-        return 1;
-    }
+    robot_cc_description_path = get_filepath_inside_pkg(package_name, rel_dir, filename);
 
     // Compose affordance screw
-    const bool aff_from_tag = true;
-    /* const Eigen::Vector3d w_aff(1, 0, 0);   // To be hard-coded as needed */
-    const Eigen::Vector3d w_aff(-1, 0, 0); // To be hard-coded as needed
-    /* const Eigen::Vector3d q_aff(0, 0.1, 0); // To be hard-coded as needed */
-    const Eigen::Vector3d q_aff(0, 0, 0); // To be hard-coded as needed
+    const bool aff_from_tag = false; // To be hard-coded as needed
     Eigen::Matrix<double, 6, 1> aff_screw;
+
     if (aff_from_tag)
     {
         const std::string affordance_frame = "affordance_frame"; // Name of the tag frame
         const std::string reference_frame = "arm0_base_link";    // Maybe we can move the aff_from_tag portion inside
                                                                  // the class later since reference_frame info is
                                                                  // available there.
-        aff_screw = get_aff_screw_from_tag(w_aff, reference_frame, affordance_frame, tf_buffer);
+        // Set screw axis
+        const Eigen::Vector3d w_aff(-1, 0, 0); // To be hard-coded as needed
+
+        // Extract Affordance frame location from TF data
+        const Eigen::Isometry3d tag_htm = AffordanceUtilROS::get_htm(reference_frame, affordance_frame, tf_buffer);
+        const Eigen::Vector3d q_aff = tag_htm.translation();
+        std::cout << "Here is the affordance frame location. Ensure it makes sense: \n" << q_aff << std::endl;
+
+        // Compute the 6x1 screw vector
+        aff_screw = get_screw(w_aff, q_aff); // return screw axis
     }
     else
     {
-        aff_screw = get_screw(w_aff, q_aff);
-    }
+        const Eigen::Vector3d w_aff(-1, 0, 0); // To be hard-coded as needed
+        const Eigen::Vector3d q_aff(0, 0, 0);  // To be hard-coded as needed
 
-    // Pure translation edit
-    aff_screw = (Eigen::Matrix<double, 6, 1>() << 0, 0, 0, -1, 0, 0).finished();
+        // Compute the 6x1 screw vector
+        aff_screw = get_screw(w_aff, q_aff);
+
+        // Pure translation edit
+        aff_screw = (Eigen::Matrix<double, 6, 1>() << 0, 0, 0, -1, 0, 0).finished();
+    }
 
     // Set affordance goal
     /* const double aff_goal = -0.5 * M_PI; */
-    const double aff_goal = -0.29;
+    const double aff_goal = -0.29; // To be hard-coded as needed
 
-    CcAffordancePlannerRosNode ccAffordancePlannerRosNode(robot_cc_description_path, aff_screw, aff_goal);
-    // Run cc affordance planner
-    ccAffordancePlannerRosNode.run_cc_affordance_planner();
+    // Construct the planner object and run the planner
+    CcAffordancePlannerRos ccAffordancePlannerRos(robot_cc_description_path, aff_screw, aff_goal);
+    ccAffordancePlannerRos.run_cc_affordance_planner();
 
     return 0;
 }
