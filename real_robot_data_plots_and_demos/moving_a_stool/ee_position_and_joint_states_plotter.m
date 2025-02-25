def8c6f0d6db8639f076667e9d5e125513ff859c
CJans121 <90218806+CJans121@users.noreply.github.com>
2025-02-24 19:38:30 -0700

    update moving_a_stool plot font sizes

--- real_robot_data_plots_and_demos/moving_a_stool/ee_position_and_joint_states_plotter.m
+++ real_robot_data_plots_and_demos/moving_a_stool/ee_position_and_joint_states_plotter.m
@@ -160,7 +160,7 @@ end
 
 % Joint state column headers
 js_end_index = 34; % specify end to truncate saturated readings
-joint_names = {'arm0_shoulder_yaw', 'arm0_shoulder_pitch', 'arm0_elbow_pitch', 'arm0_elbow_roll', 'arm0_wrist_pitch', 'arm0_wrist_roll'};
+joint_names = {'shoulder_yaw', 'shoulder_pitch', 'elbow_pitch', 'elbow_roll', 'wrist_pitch', 'wrist_roll'};
 
 % Extract data and plot
 for i = 1:length(joint_names)
@@ -176,7 +176,7 @@ grid on
 % Set fontsizes for various plot parameters
 title_fontsize = 50;
 label_fontsize = 50;
-legend_fontsize = 25;
+legend_fontsize = 40;
 tick_fontsize = 50;
 grid_lw = 1.5;
 
@@ -201,8 +201,8 @@ ax_joint_states.GridLineWidth = grid_lw;
 
 % Set legend position
 legend_position = ax_joint_states.Legend.Position;
-shift_amount_x = 0.245; 
-shift_amount_y = 0.245; 
+shift_amount_x = 0.265; 
+shift_amount_y = 0.28; 
 ax_joint_states.Legend.Position = [legend_position(1) + shift_amount_x, legend_position(2)+ shift_amount_y, legend_position(3), legend_position(4)];
 
 % Plot Error between predicted and actual EE cartesian trajectory
@@ -287,7 +287,7 @@ plot(ax_ee_traj_final, df_pred{:, 'PredEEX'} * 1000, ...
     df_pred{:, 'PredEEY'} * 1000, ...
     'b-o', 'LineWidth', 8, 'MarkerSize', 15, 'DisplayName', 'predicted trajectory');
 point_labels = 1:1:length(df_pred{:, 'PredEEX'} );
-point_label_font_size = 15;
+point_label_font_size = 25;
 labelpoints(df_pred{:, 'PredEEX'} * 1000, ...
      df_pred{:, 'PredEEY'} * 1000, point_labels,'SE', 0.4, 1, 'Fontsize', point_label_font_size, 'Color', 'b');
 hold on;
@@ -317,7 +317,7 @@ hLegend.Color = 'white'; % Set the background color of the legend box
 % Set fontsizes for various plot parameters
 title_fontsize = 35;
 label_fontsize = 35;
-legend_fontsize = 25;
+legend_fontsize = 35;
 tick_fontsize = 30;
 grid_lw = 1.5;
 
@@ -339,8 +339,8 @@ ax_ee_traj_final.Legend.FontSize = legend_fontsize;
 
 % Set legend position
 legend_position = ax_ee_traj_final.Legend.Position;
-shift_amount_x = -0.10; 
-shift_amount_y = -0.38; 
+shift_amount_x = 0.05; 
+shift_amount_y = -0.255; 
 ax_ee_traj_final.Legend.Position = [legend_position(1) + shift_amount_x, legend_position(2)+ shift_amount_y, legend_position(3), legend_position(4)];
 
 
